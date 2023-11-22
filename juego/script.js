578ca88f512c61fd99b83d918471092754109794
JVPpolitecnics <jvickeryp2223@gmail.com>
2023-11-22 20:05:13 +0100

    working well

--- juego/script.js
+++ juego/script.js
@@ -1,8 +1,13 @@
-let moveRate = 10;
+let moveRate = 2;
 let object1 = document.getElementById("p1");
-let arrayGameBoard = initializeGameBoard2DArray();
-let arrayPlanePosition = initializePlane2DArray();  
 let image = document.getElementById("planeimg");
+
+if(getCookie("country")== null || getCookie("country")== 5){
+    setCookie("country", 1, 1);
+}
+
+let countryToGoTo = getCookie("country");
+
 
 
 let direction = {
@@ -11,11 +16,15 @@ let direction = {
     left: false,
     right: false
 }
-
 let position = {
     x: 10,
     y: 10
 };
+
+startTimer();
+
+
+
 window.addEventListener("keydown", function (event) {
 
     if (event.code === "ArrowDown") {
@@ -74,6 +83,7 @@ function checkDirection() {
         image.src = "./img/planeDownLeft.png";
         direction.left = false;
         direction.down = false;
+
     }
 
 }
@@ -86,88 +96,81 @@ function refreshPosition() {
     object1.style.top = y + "px";
 }
 
-
-
-function paintTiles(gameBoardArray) {
-
-    const worldMap = document.getElementById("worldMap");
-
-// Loop through the 2D array and create div elements with img tags
-    for (let row = 0; row < gameBoardArray.length; row++) {
-        let div = document.createElement("div");
-        for (let column = 0; column < gameBoardArray[row].length; column++) {
-           
-            // Create a new img element
-            const img = document.createElement("img");
-
-         if (gameBoardArray[row][column] == "border"){
-             img.src = `img/worldTiles/black.png`;
-
-         } else if (gameBoardArray[row][column] == "plane") {
-            img.src = `img/worldTiles/planeUp.png`;
-         } else if (gameBoardArray[row][column] == "empty") { 
-
-         } else {
-             // Set the src attribute of the img tag based on the array value
-             img.src = `img/worldTiles/${gameBoardArray[row][column]}.png`;
-             }
- // Append the img element to the div
- div.appendChild(img);
-
- // Append the div to the container
- worldMap.appendChild(div);
-        }
-    }
+function startTimer(){
+    setInterval(function() {
+        checkCollisionWithCountry();  
+        setFlag(); 
+    }, 500);
 }
-function initializePlane2DArray(){
-    const array = [
-        ["border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "plane", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "border"],
-        ["border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border"],
-      ];
-      
-      
+
+function checkCollisionWithCountry(){
+let tolerance = 2;
+    let countryCoordinates = [
+    [142, 128],
+    [119, 229],
+    [103, 275],
+    [69, 187]
+];
+
+if((position.y <= countryCoordinates[countryToGoTo-1][0] + tolerance &&
+    position.y >= countryCoordinates[countryToGoTo-1][0] - tolerance) &&
+    (position.x <= countryCoordinates[countryToGoTo-1][1] + tolerance &&
+    position.x <= countryCoordinates[countryToGoTo-1][1] - tolerance)){
+
+sendToNewScreen();
+countryToGoTo++
+setCookie("country", countryToGoTo, 1);
+
 }
-    function initializeGameBoard2DArray() {
-        const array = [
-            ["border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border"],
-            ["border", "0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100", "110", "120", "130", "140", "150", "border"],
-            ["border", "1", "11", "21", "31", "41", "51", "61", "71", "81", "91", "101", "111", "121", "131", "141", "151", "border"],
-            ["border", "2", "12", "22", "32", "42", "52", "62", "72", "82", "92", "102", "112", "122", "132", "142", "152", "border"],
-            ["border", "3", "13", "23", "33", "43", "53", "63", "73", "83", "93", "103", "113", "123", "133", "143", "153", "border"],
-            ["border", "4", "14", "24", "34", "44", "54", "64", "74", "84", "94", "104", "114", "124", "134", "144", "154", "border"],
-            ["border", "5", "15", "25", "35", "45", "55", "65", "75", "85", "95", "105", "115", "125", "135", "145", "155", "border"],
-            ["border", "6", "16", "26", "36", "46", "56", "66", "76", "86", "96", "106", "116", "126", "136", "146", "156", "border"],
-            ["border", "7", "17", "27", "37", "47", "57", "67", "77", "87", "97", "107", "117", "127", "137", "147", "157", "border"],
-            ["border", "8", "18", "28", "38", "48", "58", "68", "78", "88", "98", "108", "118", "128", "138", "148", "158", "border"],
-            ["border", "9", "19", "29", "39", "49", "59", "69", "79", "89", "99", "109", "119", "129", "139", "149", "159", "border"],
-            ["border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border", "border"],
-          ];
-          return array
+
+}
+
+function sendToNewScreen(){
+
+    switch (countryToGoTo) {
+        case "1":
+            window.location.href = "brazil.html"; 
+          break;
+        case "2":
+            window.location.href = "kenya.html"; 
+          break;
+        case "3":
+            window.location.href = "india.html"; 
+          break;
+        case "4":
+            window.location.href = "europe.html"; 
+          break;
+        default:
+            setCookie("country", 1, 1);
+}
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
     }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
 
-    myInterval = setInterval(getPoint(), 1000);
-
-
-    function getPoint() {
-
-
-        alert(
-            " (" + object1.offsetLeft + ", "
-            + object1.offsetTop + ")");
-
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
+    return null;
+}
 
-    function propagateTiles() {
-       
-        paintTiles(arrayGameBoard);
-    }
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+function setFlag(){
+    let sources = ["br.png", "ke.png", "in.png", "bar.png"];
+    let flag = document.getElementById("flag");
+    flag.src = "img/"+ sources[countryToGoTo-1];
+}
